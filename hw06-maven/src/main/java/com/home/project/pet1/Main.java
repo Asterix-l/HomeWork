594369b1d62e33c3fd99a3006fd9d06732ba1bb2
Asterix-l <freepost2011@yandex.ru>
2021-03-02 18:29:18 +0300

    Done HW-6

--- hw06-maven/src/main/java/com/home/project/pet1/Main.java
+++ hw06-maven/src/main/java/com/home/project/pet1/Main.java
@@ -1,5 +1,10 @@
 package com.home.project.pet1;
 
+import com.home.project.pet1.annotation.Before;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException {
@@ -8,6 +13,21 @@ public class Main {
     }
 
     public static void testRunner(Class testClass){
-
+        int tests = 0;
+        int passed = 0;
+        for (Method m : testClass.getDeclaredMethods()) {
+            m.getAnnotations();
+            try {
+                m.invoke(testClass.getConstructor().newInstance());
+            } catch (NoSuchMethodException | InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.printf("Passed: %d, Failed: %d%n",
+                passed, tests - passed);
     }
 }
